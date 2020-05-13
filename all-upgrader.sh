a1ce5ab79d55160c4633c855121d4e83a4bf6415
Paolo Laurenti <laurentipaolo@gmail.com>
2020-05-13 14:31:35 +0200

    fix path issues

--- all-upgrader.sh
+++ all-upgrader.sh
@@ -1,22 +1,31 @@
 #!/bin/bash
-set -Eeuo pipefail
+set -Eeo pipefail
 
 trap "exit" INT TERM ERR
 trap "kill 0" EXIT
 
-sudo apt-get clean \
-&& sudo apt-get update \
-&& sudo apt-get dist-upgrade \
-&& sudo apt-get autoremove 
+source="${BASH_SOURCE[0]}"
+while [ -h "$source" ]; do # resolve $source until the file is no longer a symlink
+  DIR="$( cd -P "$( dirname "$source" )" >/dev/null 2>&1 && pwd )"
+  source="$(readlink "$source")"
+  [[ $source != /* ]] && source="$DIR/$source" # if $source was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+done
+readonly CURRENT_DIR="$( cd -P "$( dirname "$source" )" >/dev/null 2>&1 && pwd )"
+echo $CURRENT_DIR
 
-sudo snap refresh 
+# sudo apt-get clean \
+# && sudo apt-get update \
+# && sudo apt-get dist-upgrade \
+# && sudo apt-get autoremove 
 
-applications=$(ls -d applications-to-upgrade/*)
+# sudo snap refresh 
+
+applications=$(ls -d $CURRENT_DIR/applications-to-upgrade/*)
 for app in $applications
 do
   echo "======================================================"
   echo "Update $app"
-  ./$app
+  $app $CURRENT_DIR
   echo "======================================================"
   echo ""
 done
--- applications-to-upgrade/k9s.sh
+++ applications-to-upgrade/k9s.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 set -Eeuo pipefail
 
+readonly CURRENT_DIR=${1:-$(pwd)/..}
+echo $CURRENT_DIR
 readonly temp_folder=k9s_extr
 readonly package_name=k9s_Linux_x86_64.tar.gz
 
 rm -fr $temp_folder
 rm -fr $package_name
 mkdir $temp_folder
-core/github-package-retriever.sh derailed k9s $package_name
+$CURRENT_DIR/core/github-package-retriever.sh derailed k9s $package_name
 tar zxf $package_name -C "$temp_folder/"
 sudo mv -f "$temp_folder/k9s" /usr/bin/k9s
 rm -fr $temp_folder
