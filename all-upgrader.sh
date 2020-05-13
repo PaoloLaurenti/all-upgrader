f8ae53442e4f9d4e6ee900d5af19d340d5181d99
Paolo Laurenti <laurentipaolo@gmail.com>
2020-05-13 13:03:38 +0200

    uncomment code that was commentes as a mistake

--- all-upgrader.sh
+++ all-upgrader.sh
@@ -4,12 +4,12 @@ set -Eeuo pipefail
 trap "exit" INT TERM ERR
 trap "kill 0" EXIT
 
-# sudo apt-get clean \
-# && sudo apt-get update \
-# && sudo apt-get dist-upgrade \
-# && sudo apt-get autoremove 
+sudo apt-get clean \
+&& sudo apt-get update \
+&& sudo apt-get dist-upgrade \
+&& sudo apt-get autoremove 
 
-# sudo snap refresh 
+sudo snap refresh 
 
 applications=$(ls -d applications-to-upgrade/*)
 for app in $applications
