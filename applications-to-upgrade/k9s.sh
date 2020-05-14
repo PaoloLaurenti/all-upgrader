025fb2440eb67d9b52ba23a9cfbf888e025ee7ba
Paolo Laurenti <laurentipaolo@gmail.com>
2020-05-14 19:26:26 +0200

    change github package retriever in oreder to filter release name with a regex

--- applications-to-upgrade/k9s.sh
+++ applications-to-upgrade/k9s.sh
@@ -3,13 +3,15 @@ set -Eeuo pipefail
 
 readonly CURRENT_DIR=${1:-$(pwd)/..}
 readonly temp_folder=k9s_extr
-readonly package_name=k9s_Linux_x86_64.tar.gz
+readonly package_name_regex="^k9s_Linux_x86_64.tar.gz$"
+readonly github_owner=derailed
+readonly github_repo=k9s
 
 rm -fr $temp_folder
-rm -fr $package_name
+ls | grep -P "$package_name_regex" | xargs -d"\n" rm
 mkdir $temp_folder
-$CURRENT_DIR/core/github-package-retriever.sh derailed k9s $package_name
-tar zxf $package_name -C "$temp_folder/"
+$CURRENT_DIR/core/github-package-retriever.sh $github_owner $github_repo $package_name_regex
+ls | grep -P "$package_name_regex" | xargs cat - | tar zxf - -C "$temp_folder/"
 sudo mv -f "$temp_folder/k9s" /usr/bin/k9s
 rm -fr $temp_folder
-rm -fr $package_name
+ls | grep -P "$package_name_regex" | xargs -d"\n" rm
--- core/github-package-retriever.sh
+++ core/github-package-retriever.sh
@@ -3,9 +3,9 @@ set -Eeuo pipefail
 
 readonly owner=$1
 readonly repository=$2
-readonly package_name=$3
+readonly package_name_regex=$3
 
 url=$(curl -X GET https://api.github.com/repos/$owner/$repository/releases/latest \
-  | jq -r --arg name "$package_name" '.assets | .[] | select(.name | contains($name)) | .browser_download_url')
+  | jq -r --arg name_regex "$package_name_regex" '.assets | .[] | select(.name | test($name_regex)) | .browser_download_url')
 
 wget "$url"
