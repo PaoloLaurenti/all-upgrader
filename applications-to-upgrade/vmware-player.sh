8f0f9b410bf6858716bb25490ed4bfa87177e48c
Paolo Laurenti <laurentipaolo@gmail.com>
2020-07-07 08:21:13 +0200

    fix vmware installation script

--- applications-to-upgrade/vmware-player.sh
+++ applications-to-upgrade/vmware-player.sh
@@ -9,7 +9,7 @@ while [ -h "$source" ]; do # resolve $source until the file is no longer a symli
 done
 readonly CURRENT_DIR="$( cd -P "$( dirname "$source" )" >/dev/null 2>&1 && pwd )"
 readonly temp_folder=wmware_temp
-readonly package_name="getplayer-linux"
+readonly package_name="getplayer-linux.bundle"
 
 temp_folder_path="$CURRENT_DIR/$temp_folder"
 package_name_path="$temp_folder_path/$package_name"
@@ -19,6 +19,6 @@ mkdir -p $temp_folder_path
 
 wget -O $package_name_path --user-agent="Mozilla/5.0 (X11; Linux x86_64; rv:75.0) Gecko/20100101 Firefox/75.0" https://www.vmware.com/go/getplayer-linux
 chmod +x $package_name_path 
-sudo $package_name_path
+sudo vmware-installer --console -i $package_name_path
 
 rm -fr $temp_folder_path
