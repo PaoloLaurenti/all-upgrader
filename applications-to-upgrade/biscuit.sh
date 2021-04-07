7df240102c56ef395e5aaddca1adc17a7fb0a608
Paolo Laurenti <laurentipaolo@gmail.com>
2021-04-07 14:19:18 +0200

    add biscuit application

---
+++ applications-to-upgrade/biscuit.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+set -Eeuo pipefail
+
+source="${BASH_SOURCE[0]}"
+while [ -h "$source" ]; do # resolve $source until the file is no longer a symlink
+  DIR="$( cd -P "$( dirname "$source" )" >/dev/null 2>&1 && pwd )"
+  source="$(readlink "$source")"
+  [[ $source != /* ]] && source="$DIR/$source" # if $source was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+done
+readonly CURRENT_DIR="$( cd -P "$( dirname "$source" )" >/dev/null 2>&1 && pwd )"
+
+readonly temp_folder=biscuit_temp
+readonly extr_folder=biscuit_extr
+readonly package_name="biscuit.tar.gz"
+readonly package_name_regex="^biscuit-linux_386.tgz$"
+readonly github_owner=primait
+readonly github_repo=biscuit
+readonly url_json_field_name=browser_download_url
+
+temp_folder_path="$CURRENT_DIR/$temp_folder"
+extr_folder_path="$temp_folder_path/$extr_folder"
+package_name_path="$temp_folder_path/$package_name"
+
+rm -fr $temp_folder_path
+mkdir -p $temp_folder_path
+mkdir -p $extr_folder_path
+
+$CURRENT_DIR/../core/github-package-retriever-by-assets.sh $github_owner $github_repo $package_name_regex $url_json_field_name $package_name_path
+tar zxf $package_name_path -C "$extr_folder_path/"
+chmod +x $extr_folder_path/biscuit
+sudo mv -f "$extr_folder_path/biscuit" /usr/local/bin/biscuit
+
+rm -fr $temp_folder_path
